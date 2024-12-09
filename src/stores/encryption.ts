dbd96c7f8a5602f5d2947e080a3f25f2a658815a
Gaston Gaiduk <gastongaiduk@gmail.com>
2024-12-09 20:41:13 +0100

    Fix encryption

--- src/stores/encryption.ts
+++ src/stores/encryption.ts
@@ -1,10 +1,7 @@
 import { EncryptStorage } from 'encrypt-storage';
 
-// Initialize EncryptStorage with your secret key from environment variables
 const encryptStorage = new EncryptStorage(import.meta.env.VITE_API_URL as string, {
-    prefix: '@retroleaderboards', // Optional: Prefix for your keys
-    storageType: 'localStorage', // Specify storage type
+    storageType: 'localStorage'
 });
 
-// Export the instance for use throughout your application
 export default encryptStorage;
