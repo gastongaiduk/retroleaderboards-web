774cf8932b875b9de9ef19201b0e4444235ab0df
Gaston Gaiduk <gastongaiduk@gmail.com>
2024-12-10 19:59:29 +0100

    Fix missing env variable

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,10 +1,10 @@
 import CryptoJS from 'crypto-js';
 
 export function encryptData(data: string): string {
-    return CryptoJS.AES.encrypt(data, import.meta.env.VITE_ENCRIPTION_KEY as string).toString();
+    return CryptoJS.AES.encrypt(data, import.meta.env.VITE_ENCRYPTION_KEY as string).toString();
 }
 
 export function decryptData(ciphertext: string): string {
-    const bytes = CryptoJS.AES.decrypt(ciphertext, import.meta.env.VITE_ENCRIPTION_KEY as string);
+    const bytes = CryptoJS.AES.decrypt(ciphertext, import.meta.env.VITE_ENCRYPTION_KEY as string);
     return bytes.toString(CryptoJS.enc.Utf8);
 }
