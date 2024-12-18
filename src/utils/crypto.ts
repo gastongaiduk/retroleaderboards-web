19068a9f6a6ff8aa243d3dcce069ed7e8c0e3b41
Gaston Gaiduk <gastongaiduk@gmail.com>
2024-12-18 15:37:23 +0100

    Last approach

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,10 +1,11 @@
 import CryptoJS from 'crypto-js';
 
 export function encryptData(data: string): string {
-    return CryptoJS.AES.encrypt(data, import.meta.env.VITE_ENCRYPTION_KEY as string).toString();
+    const encryptionKey = import.meta.env.VITE_ENCRYPTION_KEY as string || 'test_key';
+    return CryptoJS.AES.encrypt(data, encryptionKey).toString();
 }
 
 export function decryptData(ciphertext: string): string {
-    const bytes = CryptoJS.AES.decrypt(ciphertext, import.meta.env.VITE_ENCRYPTION_KEY as string);
+    const bytes = CryptoJS.AES.decrypt(ciphertext, import.meta.env.VITE_ENCRYPTION_KEY as string || 'test_key');
     return bytes.toString(CryptoJS.enc.Utf8);
 }
