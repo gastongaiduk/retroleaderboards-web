12296f8c739acc88ff851cebf9b360df565dfcb2
Gaston Gaiduk <gastongaiduk@gmail.com>
2024-12-18 15:03:07 +0100

    Tests adjustments

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: process.env.BASE_URL || 'http://localhost:5173/retroleaderboards',
+    baseUrl: 'http://localhost:5173/retroleaderboards',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
